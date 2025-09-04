edccfb94a62139b5eaa6bc47cca1bb6567868316
lelyagabov <lelya_gabov@mail.ru>
2025-09-04 14:44:55 +0500

    task finished

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character == 65 || character == 69 || character == 73 || character == 79 || character == 85 || character == 89
+        || character == 97 || character == 101 || character == 105 || character == 111 || character == 117 || character == 121) {
+            System.out.println("Vowel");
+        } else if (character > 65 && character < 69 || character >= 70 && character <= 72 || character >= 74 &&
+        character <= 78 || character >= 80 && character <= 84 && character > 85 && character < 89 || character == 90 ||
+        character > 97 && character < 101 || character >= 102 && character <= 104 || character > 105 && character < 111 ||
+        character > 111 && character < 117 || character >= 118 && character <= 120 || character == 122) {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary > 0 && salary <= 10000) {
+            System.out.println(salary - (salary * 15 / 100.0));
+        } else if (salary > 10000 && salary <= 20000) {
+            System.out.println(salary - (salary * 18 / 100.0));
+        } else if (salary > 20000) {
+            System.out.println(salary - (salary * 20 / 100.0));
+        } else {
+            System.out.println("wrong input!");
+        }
+    }
+
+    public static void main(String[] args) {
+        SalaryCalculator calculator = new SalaryCalculator();
+
+        calculator.calculateSalary(5000);
     }
 }
